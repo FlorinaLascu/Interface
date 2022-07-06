c6f86a76475c240a5f9db091eaf9ddcd594db6a9
florinalascu <florina.lascu@yahoo.com>
2022-07-06 13:19:24 +0300

    second commit

--- src/Rabbit.java
+++ src/Rabbit.java
@@ -12,11 +12,20 @@ public class Rabbit implements Prey{
     }
     @Override
     public void flee() {
+
         System.out.println("The Rabbit is fleeing");
     }
 
     @Override
     public  String toString(){
         return "NUME: "+ this.nume+ " GREUTATE: " + this.greutate;
+    }
+
+    public int getGreutate() {
+        return greutate;
+    }
+
+    public String getNume() {
+        return nume;
     }
 }
